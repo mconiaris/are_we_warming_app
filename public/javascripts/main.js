dea954ce86076b2329a364aad9411f8e249b39d9
mconiaris <mconiaris@gmail.com>
2014-10-26 16:14:53 -0400

    Added csv file.

---
+++ public/javascripts/main.js
@@ -0,0 +1,15 @@
+console.log('main.js linked');
+
+$(document).ready(function(){
+
+  g = new Dygraph(
+
+    // containing div
+    document.getElementById("graphdiv"),
+
+    // CSV or path to a CSV file.
+    "/data/usa_temps.csv"
+
+  );
+
+});
