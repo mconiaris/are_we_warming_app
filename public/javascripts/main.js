4c9cec9d60a6ee1aacad8426405bc6e852e14c3d
mconiaris <mconiaris@gmail.com>
2014-10-27 10:31:03 -0400

    More changes to CSV. Dealing with errors.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,7 +8,19 @@ $(document).ready(function(){
     document.getElementById("graphdiv"),
 
     // CSV or path to a CSV file.
-    "/data/usa_temps.csv"
+    "/data/debt_table.CSV",
+    { labels: [
+      "Fiscal Year",
+      "Receipts In Current Dollars",
+      "Outlays In Current Dollars",
+      "Surplus or Deficit (-) In Current Dollars",
+      "Receipts In Constant (FY2009 Dollars)",
+      "Outlays In Constant (FY2009 Dollars)",
+      "Surplus or Deficit (-) In Constant (FY2009 Dollars)",
+      "Addendum: Composite Deflator",
+      "Receipts As Percentages of GDP",
+      "Outlays As Percentages of GDP",
+      "Surplus or Deficit (-) As Percentages of GDP" ]}
   );
 
 });
